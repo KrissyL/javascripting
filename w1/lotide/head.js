347b5e5d85f8bd032dbbf4b82bfd25314d5ad93e
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-20 01:54:57 +0000

    lint cleanup

--- w1/lotide/head.js
+++ w1/lotide/head.js
@@ -1,13 +1,13 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
-    } else if (actual !== expected) {
-      console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+  if (actual === expected) {
+    console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
-const head = function(arr) { 
-    return arr[0];
+const head = function(arr) {
+  return arr[0];
 };
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
